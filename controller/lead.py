95aec57222b4bee7153268e34e2e3ae132d872da
SathishtonyC <128224891+SathishtonyC@users.noreply.github.com>
2023-03-23 20:50:14 +0000

    Lead feature (#1)
    *full Lead feature done and tested

--- controller/lead.py
+++ controller/lead.py
@@ -1,1 +1,17 @@
-lead.py
+#lead.py
+#lead config test
+
+def create_local():
+	print "load creation done"
+
+#update lead 
+def update_lead():
+	print "lead update done."
+
+#get lead
+def get_lead():
+	print "lead get done."
+
+#get lead
+def delete_lead():
+	print "lead delete done."
